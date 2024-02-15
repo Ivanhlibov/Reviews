6b8492828d96e65594958ab9f19d039a6da2b312
Ivanhlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-02-15 18:28:45 +0200

    end project

--- reviews/src/Components/Reviews.jsx
+++ reviews/src/Components/Reviews.jsx
@@ -1,34 +1,33 @@
+
 import React, { Component } from "react";
 
 class App extends Component{
     state = {
         good: 0,
         neytral: 0,
-        bad:0,
+        bad: 0,
     }
-    
-handlefeedback = (type) => {
-    this.setState(prevState => ({
-        [type]: prevState[type] + 1
-    }));
+
+    handleFeedBack = (type) => {
+        this.setState(preState => ({
+            [type]: preState[type] + 1
+        }) )
+    }
+    render(){
+        const {good, neytral, bad} = this.state;
+        return(
+            <div>
+                <h1>Відгуки</h1>
+                <button onClick={()=> this.handleFeedBack('good') } >Good</button>
+                <button onClick={()=> this.handleFeedBack('Neytral') } >Neytral</button>
+                <button onClick={()=> this.handleFeedBack('bad') } >Bad</button>
+                <h2>Все буде Good</h2>
+                <p>Good:{good}</p>
+                <p>Neytral:{neytral}</p>
+                <p>Bad:{bad}</p>
+            </div>
+        )
+    }
 }
 
-render(){
-    const {good, neytral, bad} = this.state;
-    return(
-        <div>
-        <h1>Відгуки</h1>
-        <button onClick={()=> this.handlefeedback('good')} >good</button>
-        <button onClick={()=> this.handlefeedback('neytral')} >neytral</button>
-        <button onClick={()=> this.handlefeedback('bad')} >bad</button>
-        <h2>У тебя все получиться!</h2>
-        <p>Добре: {good}</p>
-        <p>Нейтрально: {neytral}</p>
-        <p>Добре: {bad}</p>
-        </div>
-    )
-  }
-}
-
-
-export default App;
+export default App
